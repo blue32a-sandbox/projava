f5374e5093dec30ee554150d07d4b5e10717ab62
blue32a <blue32a.jp@gmail.com>
2022-06-01 00:01:21 +0900

    12.1.5 try句で例外に対処する

--- src/main/java/projava/ReadFile.java
+++ src/main/java/projava/ReadFile.java
@@ -2,12 +2,22 @@ package projava;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
-        var p = Path.of("test.txt");
-        String s = Files.readString(p);
-        System.out.println(s);
+        read("test.txt");
+        read("testa.txt");
+    }
+
+    private static void read(String name) throws IOException {
+        try {
+            var p = Path.of(name);
+            String s = Files.readString(p);
+            System.out.println(s);
+        } catch (NoSuchFileException e) {
+            System.out.println("ファイルが見つかりません：" + e.getFile());
+        }
     }
 }
