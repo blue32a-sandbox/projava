97b62d4376a4ba061a97ee0c9aab55a007c2203b
blue32a <blue32a.jp@gmail.com>
2022-06-04 23:06:36 +0900

    14.4 クラスとファイル

---
+++ src/main/java/projava/ParamSample.java
@@ -0,0 +1,20 @@
+package projava;
+
+import java.util.Arrays;
+
+public class ParamSample {
+    /**
+     * プログラム開始時に呼び出される
+     * @param args コマンドライン引数
+     */
+    public static void main(String[] args) {
+        // 引数の内容を表示（配列は直接表示できないのでArraysのメソッドを使う）
+        System.out.println(Arrays.toString(args));
+    }
+}
+
+/*
+ publicではないアウタークラス
+ 複数のアウタークラスをひとつのファイルに定義できることを示すため
+ */
+class Dummy {}
