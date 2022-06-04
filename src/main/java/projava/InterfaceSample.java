a5b22ae7290d415d846ebf480e75724a71b5900d
blue32a <blue32a.jp@gmail.com>
2022-06-04 22:48:20 +0900

    14.2 インタフェース

---
+++ src/main/java/projava/InterfaceSample.java
@@ -0,0 +1,33 @@
+package projava;
+
+import java.util.List;
+
+public class InterfaceSample {
+    interface Named {
+        String name();
+
+        default String greeting() {
+            return "こんにちは%sさん".formatted(name());
+        }
+    }
+
+    record Student(String name, int score) implements Named {}
+    record Teacher(String name, String subject) implements Named {}
+
+    static class Passenger implements Named {
+        @Override
+        public String name() {
+            return "通りすがり";
+        }
+    }
+
+    public static void main(String[] args) {
+        var people = List.of(
+                new Student("kis", 80),
+                new Teacher("hosoya", "Math"),
+                new Passenger());
+        for (Named p : people) {
+            System.out.println(p.greeting());
+        }
+    }
+}
