b3ad9eca62daa1199850ed650221082a6ccab3be
blue32a <blue32a.jp@gmail.com>
2022-06-11 16:04:55 +0900

    18.3 オリンピック開催年を判別するコードをテスト

--- src/main/java/projava/JavadocSample.java
+++ src/main/java/projava/JavadocSample.java
@@ -2,21 +2,6 @@ package projava;
 
 public class JavadocSample {
     public static void main(String[] args) throws IllegalAccessException {
-        System.out.println(new Olympic().isSummerOlympicYear(2020));
-    }
-}
-
-class Olympic {
-    /**
-     * 渡された西暦年が夏季近代オリンピック開催年であるかどうか判定する
-     * @param year 西暦年
-     * @return 夏季オリンピック開催年であればtrue
-     * @throws IllegalAccessException まだオリンピック開催が確定していない年を渡した場合
-     */
-    public boolean isSummerOlympicYear(int year) throws IllegalAccessException {
-        if (2032 < year) {
-            throw new IllegalAccessException("2032年までサポートしています。入力：" + year);
-        }
-        return year % 4 == 0;
+        System.out.println(new Olympic().isSummerOlympicYear(2016));
     }
 }
